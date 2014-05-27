ac48ab29ec4bf8d801aa77b22263d7d7819df3fe
user name <user@email.com>
2014-05-27 12:38:11 -0400

    First change, added a sys out.

--- GitWithTwoFiles/src/TheMainFile.java
+++ GitWithTwoFiles/src/TheMainFile.java
@@ -2,5 +2,6 @@
 public class TheMainFile {
 	public static void main(String [] args) {
 		System.out.println("this is the main file");
+		System.out.println("edited after pushing to remote server");
 	}
 }
