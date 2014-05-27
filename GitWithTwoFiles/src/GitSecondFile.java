da911ba45e71238cc3a10d62421b90341a9a0756
user name <user@email.com>
2014-05-27 13:09:32 -0400

    adding for loop on GitSecondFile.java

--- GitWithTwoFiles/src/GitSecondFile.java
+++ GitWithTwoFiles/src/GitSecondFile.java
@@ -1,6 +1,10 @@
 
 public class GitSecondFile {
 	public void message() {
+		String message[] = {"Git","message"};
 		System.out.println("The second file");
+		for (int i=0;i<message.length;i++) {
+			System.out.println("message: " + message[i]);
+		}
 	}
 }
