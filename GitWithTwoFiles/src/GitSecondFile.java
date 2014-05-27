caf9d67dd80ec8f53f4f48e3408d4c52c17a778b
user name <user@email.com>
2014-05-27 13:02:45 -0400

    after editing GidSecondFiles.java, added a for loop

--- GitWithTwoFiles/src/GitSecondFile.java
+++ GitWithTwoFiles/src/GitSecondFile.java
@@ -1,7 +1,11 @@
 
 public class GitSecondFile {
 	public void message() {
+		String message[]	 = {"Git","message"};
 		System.out.println("The second file");
 		System.out.println("after creating firstBranch, added the sys out");
+		for (int i=0;i<message.length;i++) {
+			System.out.println("message: " + message[i]);
+		}
 	}
 }
