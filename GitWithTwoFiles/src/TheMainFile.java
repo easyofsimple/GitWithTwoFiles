6285b933174aad82dfd0ec175758821c5f02d77b
user name <user@email.com>
2014-05-27 12:54:37 -0400

    Added sys out on main file and second file.

--- GitWithTwoFiles/src/TheMainFile.java
+++ GitWithTwoFiles/src/TheMainFile.java
@@ -1,7 +1,9 @@
 
 public class TheMainFile {
 	public static void main(String [] args) {
-		System.out.println("this is the main file");
-		System.out.println("edited after pushing to remote server");
+		
+		System.out.println("Hello => this is the main file");
+		System.out.println("-------------------------------------------------");
+		System.out.println("this will show message : edited after pushing to remote server");
 	}
 }
